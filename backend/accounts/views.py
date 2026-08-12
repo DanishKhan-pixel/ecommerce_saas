0badfed009d60b4a7387a2081ab35d88f1505030
MUHAMMAD DANISH <96936557+DanishKhan-pixel@users.noreply.github.com>
2026-08-12 10:03:01 +0500

    nnecessary blank line in views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -4,7 +4,6 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from .serializers import RegisterSerializer, UserSerializer
 
 @api_view(['POST'])
