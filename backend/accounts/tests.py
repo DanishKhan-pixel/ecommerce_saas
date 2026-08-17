2255fbfed5ed58a9d483f6f32cb0c9a7c3ee63c5
MUHAMMAD DANISH <96936557+DanishKhan-pixel@users.noreply.github.com>
2026-08-17 22:00:10 +0500

    remove commented-out AdminSiteTests class from accounts tests

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -2,13 +2,3 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from .models import CustomUser
-
-class AdminSiteTests(TestCase):
-    def setUp(self):
-        self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@example.com',
-            password='testpass123'
-        )
-        self.client.login(email='admin@example.com', password='testpass123')
-        
-        self.user = get_user_model().objects.create_user(
